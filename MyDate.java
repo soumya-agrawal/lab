c305eb663dd2ee9f82640c466a3ffa3f0c0ecb0f
soumya-agrawal <75248956+soumya-agrawal@users.noreply.github.com>
2020-12-09 16:45:06 +0530

    Create MyDate.java

---
+++ MyDate.java
@@ -0,0 +1,10 @@
+public class MyDate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mydate class");
+		System.out.println("Static method");
+
+	}
+
+}
